609b13c23d0451c0d4dde8059658ee23a6280168
Jacek Noga <jaceknoga11@gmail.com>
2021-03-23 18:17:13 +0100

    Added log4j2 logger
    (cherry picked from commit 1bd7c534d8758bf61315446ceccb5c42e6ea454d)

--- src/main/java/com/jacek/pdfmanipulator/controller/MergeController.java
+++ src/main/java/com/jacek/pdfmanipulator/controller/MergeController.java
@@ -4,6 +4,8 @@ import com.jacek.pdfmanipulator.model.PdfMergeInput;
 import com.jacek.pdfmanipulator.service.FileStorageService;
 import com.jacek.pdfmanipulator.service.PdfService;
 import com.jacek.pdfmanipulator.validator.FileValidator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -15,31 +17,33 @@ import java.io.File;
 import java.nio.file.Files;
 
 @Controller
-@RequestMapping("/home")
-public class HomeController {
+@RequestMapping("/merge")
+public class MergeController {
 
     private PdfService pdfService;
     private FileStorageService fileStorageService;
 
+    private final static Logger LOG = LogManager.getLogger(MergeController.class);
+
     @Autowired
-    public HomeController(PdfService pdfService, FileStorageService fileStorageService) {
+    public MergeController(PdfService pdfService, FileStorageService fileStorageService) {
         this.pdfService = pdfService;
         this.fileStorageService = fileStorageService;
     }
 
-    @GetMapping("/merge")
+    @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("pdfMergeInput", new PdfMergeInput());
         return "index";
     }
 
-    @PostMapping(value = "/merge", produces = MediaType.APPLICATION_PDF_VALUE)
+    @PostMapping(value = "/", produces = MediaType.APPLICATION_PDF_VALUE)
     public @ResponseBody byte[] merge(@RequestParam("file1") MultipartFile uploadedFile1,
                                       @RequestParam("file2") MultipartFile uploadedFile2,
                                       @RequestParam("resultFileName") String resultFileName) throws Exception {
 
-        System.out.println(uploadedFile1.getOriginalFilename());
-        System.out.println(uploadedFile2.getOriginalFilename());
+        LOG.info("File1: " + uploadedFile1.getOriginalFilename());
+        LOG.info("File2: " + uploadedFile2.getOriginalFilename());
         fileStorageService.store(uploadedFile1);
         fileStorageService.store(uploadedFile2);
         File file1 = fileStorageService.loadFile(uploadedFile1.getOriginalFilename()).getFile();
@@ -48,18 +52,18 @@ public class HomeController {
         FileValidator file2Validator = new FileValidator(file2);
         if (!file1Validator.isValid()) {
             String message = "First file is incorrect! " + file1Validator.getMessage();
-            System.out.println(message);
+            LOG.warn(message);
             throw new IllegalStateException(message);
         }
         if (!file2Validator.isValid()) {
             String message = "Second file is incorrect! " + file2Validator.getMessage();
-            System.out.println(message);
+            LOG.warn(message);
             throw new IllegalStateException(message);
         }
 
         if (FileValidator.isEmpty(resultFileName)) {
             String message = "Result file name is empty!";
-            System.out.println(message);
+            LOG.warn(message);
             throw new IllegalStateException(message);
         }
 
@@ -74,7 +78,7 @@ public class HomeController {
 
     @ExceptionHandler(Exception.class)
     public String handleException(Model model, Exception ex) {
-        System.out.println(ex);
+        LOG.error(ex.getMessage(), ex);
         model.addAttribute("error", ex.getMessage());
         return "error";
     }
--- src/main/java/com/jacek/pdfmanipulator/scheduler/FileRemoveScheduler.java
+++ src/main/java/com/jacek/pdfmanipulator/scheduler/FileRemoveScheduler.java
@@ -1,6 +1,8 @@
 package com.jacek.pdfmanipulator.scheduler;
 
 import com.jacek.pdfmanipulator.util.FileRemoverUtil;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -19,12 +21,14 @@ public class FileRemoveScheduler {
     private final static long INTERVAL_IN_MINUTES = 5 * INTERVAL_ONE_MINUTE;
     private final static Path FILE_DIR = Paths.get("storage");
 
+    private final static Logger LOG = LogManager.getLogger(FileRemoveScheduler.class);
+
     @Scheduled(fixedRate = INTERVAL_IN_MINUTES)
     public void tick() throws IOException {
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
         LocalDateTime currentDateTime = LocalDateTime.now();
         String formattedDateTime = dateTimeFormatter.format(currentDateTime);
-        System.out.println("FileRemover started work at: " + formattedDateTime);
+        LOG.info("FileRemover started work at: " + formattedDateTime);
         Stream<Path> paths = Files.walk(FILE_DIR);
         paths.filter(p -> p != null)
                 .filter(Files::isRegularFile)
@@ -34,10 +38,10 @@ public class FileRemoveScheduler {
 
     private static void remove(Path path) {
         try {
-            System.out.println("Removing file: " + path.getFileName());
+            LOG.info("Removing file: " + path.getFileName());
             Files.delete(path);
         } catch (Exception ex) {
-            System.out.println("Removal of file: " + path.getFileName() + " failed!");
+            LOG.error("Removal of file: " + path.getFileName() + " failed!", ex);
         }
     }
 
